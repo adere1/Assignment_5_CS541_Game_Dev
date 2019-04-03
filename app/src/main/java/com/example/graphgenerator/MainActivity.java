ffc00164aa0797dd779f44780365e7f606adea46
ShriramSuryawanshi <shriram.a.suryawanshi@gmail.com>
2019-04-03 12:55:46 -0400

    UI phase 1

--- app/src/main/java/com/example/graphgenerator/MainActivity.java
+++ app/src/main/java/com/example/graphgenerator/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.graphgenerator;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.net.URLEncoder;
 
@@ -17,60 +23,57 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
 
 
-        Button submit = findViewById(R.id.submit);
-        submit.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+        final Button submit = findViewById(R.id.submit);
+       /* submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 WebView webview = findViewById(R.id.webview);
+                webview.setVisibility(View.VISIBLE);
+                submit.setVisibility(View.INVISIBLE);
                 //setContentView(webview);
-                String url = "http://149.125.136.144:8080/svg";
+                String url = "http://192.168.1.179:8080";
                 String postData = "digraph G {" +
                         "a -> b -> c;" +
                         "a-> c;" +
                         "b -> d;" +
                         "c-> d;" +
                         "}";
-                //webview.loadUrl(url);
+
+                webview.setBackgroundColor(Color.WHITE);
                 webview.postUrl(url,postData.getBytes());
             }
-        });
+        }); */
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void radioDot(View view) {
+
+        TextView textView_que1 = findViewById(R.id.textView_que1);
+        textView_que1.setText("Enter dot language input of your graph -");
+
+        EditText editText_dot = findViewById(R.id.editText_dot);
+        editText_dot.setVisibility(View.VISIBLE);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void radioNoDot(View view) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        TextView textView_que1 = findViewById(R.id.textView_que1);
+        textView_que1.setText("How many nodes your graph have?");
 
-        return super.onOptionsItemSelected(item);
+        EditText editText_dot = findViewById(R.id.editText_dot);
+        editText_dot.setVisibility(View.INVISIBLE);
     }
+
+
+
 }
