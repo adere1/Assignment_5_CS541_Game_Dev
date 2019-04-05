4342b51a6d41967092c4623b517c62248aa11055
ShriramSuryawanshi <shriram.a.suryawanshi@gmail.com>
2019-04-05 02:24:07 -0400

    Addin node inputs

--- app/src/main/java/com/example/graphgenerator/MainActivity.java
+++ app/src/main/java/com/example/graphgenerator/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.graphgenerator;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +23,20 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     boolean dot = false;
     int nodeCount = 0;
     int currentNode = 1;
-    List<String> list = new ArrayList<String>(Arrays.asList("Box", "Circle", "Diamond", "Ellipse", "Oval", "Doublecircle", "Square", "Parallelogram", "Trapezium", "Egg"));
+    List<String> list = new ArrayList<String>(Arrays.asList("Circle", "Box", "Diamond", "Ellipse", "Oval", "Doublecircle", "Square", "Parallelogram", "Trapezium", "Egg"));
 
     EditText editText_nodeCount, editText_nodeName, editText_dot, editText_myDot;
-    Button button_submitCount, button_addNode;
-    TextView textView_que1, textView_Name, textView_Shape;
-    Spinner spinner1;
+    Button button_submitCount, button_addNode, button_addEdge;
+    TextView textView_que1, textView_Name, textView_Shape, textView_toNode, textView_fromNode;
+    Spinner spinner1, spinner_fromNode, spinner_toNode;
+
+    String nodeName, nodeShape;
+    String dotGenerated;
+    int nodeID = 97;
+
+    List<String> nodeNameList = new ArrayList<>();
+    List<String> nodeShapeList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +51,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinner1.setAdapter(adapter);
         spinner1.setOnItemSelectedListener(this);
 
+        spinner_fromNode = findViewById(R.id.spinner_fromNode);
+        spinner_toNode = findViewById(R.id.spinner_toNode);
+
         textView_que1 = findViewById(R.id.textView_que1);
         textView_Name = findViewById(R.id.textView_Name);
         textView_Shape = findViewById(R.id.textView_Shape);
+        textView_fromNode = findViewById(R.id.textView_fromNode);
+        textView_toNode = findViewById(R.id.textView_toNode);
 
         editText_nodeCount = findViewById(R.id.editText_nodeCount);
         editText_nodeName = findViewById(R.id.editText_nodeName);
@@ -50,6 +66,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         editText_dot = findViewById(R.id.editText_dot);
 
         button_addNode = findViewById(R.id.button_addNode);
+        button_addEdge = findViewById(R.id.button_addEdge);
         button_submitCount = findViewById(R.id.button_submitCount);
 
         radioChange(false);
@@ -78,6 +95,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     public void radioDot(View view) {
 
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
         if( dot == false) {
             dot = true;
             radioChange(true);
@@ -85,6 +105,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     public void radioNoDot(View view) {
+
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
 
         if( dot == true) {
             dot = false;
@@ -95,20 +118,43 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     public void submitCount(View view) {
 
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
         nodeCount = Integer.parseInt(editText_nodeCount.getText().toString());
-        textView_que1.setText("Please enter details for node " + currentNode + " - ");
 
-        textView_Name.setVisibility(View.VISIBLE);
-        textView_Shape.setVisibility(View.VISIBLE);
+        if (nodeCount > 0 && nodeCount < 27) {
+            textView_que1.setText("Please enter details for node " + currentNode + ", total nodes are " + nodeCount + " - ");
 
-        spinner1.setVisibility(View.VISIBLE);
+            textView_Name.setVisibility(View.VISIBLE);
+            textView_Shape.setVisibility(View.VISIBLE);
+            textView_fromNode.setVisibility(View.INVISIBLE);
+            textView_toNode.setVisibility(View.INVISIBLE);
 
-        editText_nodeCount.setVisibility(View.INVISIBLE);
-        editText_myDot.setVisibility(View.VISIBLE);
-        editText_nodeName.setVisibility(View.VISIBLE);
+            spinner1.setVisibility(View.VISIBLE);
+            spinner_fromNode.setVisibility(View.INVISIBLE);
+            spinner_toNode.setVisibility(View.INVISIBLE);
 
-        button_submitCount.setVisibility(View.INVISIBLE);
-        button_addNode.setVisibility(View.VISIBLE);
+            editText_nodeCount.setVisibility(View.INVISIBLE);
+            editText_myDot.setVisibility(View.VISIBLE);
+            editText_nodeName.setVisibility(View.VISIBLE);
+
+            button_submitCount.setVisibility(View.INVISIBLE);
+            button_addNode.setVisibility(View.VISIBLE);
+            button_addEdge.setVisibility(View.INVISIBLE);
+
+            dotGenerated = "";
+            editText_nodeName.setText("");
+            editText_myDot.setText("");
+
+            nodeID = 97;
+            currentNode = 1;
+
+            nodeNameList.clear();
+            nodeShapeList.clear();
+        }
+        else
+            Toast.makeText(this, "Total nodes should be between 1 and 27!", Toast.LENGTH_LONG).show();
     }
 
 
@@ -117,12 +163,18 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         textView_Name.setVisibility(View.INVISIBLE);
         textView_Shape.setVisibility(View.INVISIBLE);
+        textView_fromNode.setVisibility(View.INVISIBLE);
+        textView_toNode.setVisibility(View.INVISIBLE);
 
         editText_nodeName.setVisibility(View.INVISIBLE);
         editText_myDot.setVisibility(View.INVISIBLE);
 
         spinner1.setVisibility(View.INVISIBLE);
+        spinner_fromNode.setVisibility(View.INVISIBLE);
+        spinner_toNode.setVisibility(View.INVISIBLE);
+
         button_addNode.setVisibility(View.INVISIBLE);
+        button_addEdge.setVisibility(View.INVISIBLE);
 
         if (dotSelected) {
 
@@ -138,22 +190,73 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             editText_nodeCount.setVisibility(View.VISIBLE);
             button_submitCount.setVisibility(View.VISIBLE);
         }
-
     }
 
 
 
     public void submitNode(View view) {
 
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
         if (currentNode <= nodeCount) {
 
+            nodeName = editText_nodeName.getText().toString();
+
+            if(nodeName.length() != 0) {
+
+                textView_que1.setText("Please enter details for node " + currentNode + ", total nodes are " + nodeCount + " - ");
+                currentNode++;
+
+                dotGenerated = dotGenerated + "\nNode ID: " + (char) nodeID + ", \tName: " + nodeName + ", \tShape: " + nodeShape;
+                nodeID++;
+
+                nodeNameList.add(nodeName);
+                nodeShapeList.add(nodeShape);
+
+                editText_myDot.setText(dotGenerated);
+                editText_nodeName.setText("");
+            }
+            else
+                Toast.makeText(this, "Please enter node name!", Toast.LENGTH_LONG).show();
+        }
+        else {
+
+            dotGenerated = dotGenerated + "\n\n";
+
+            ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, R.layout.spinner_item, nodeNameList);
+            adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner_fromNode.setAdapter(adapter1);
+            spinner_fromNode.setOnItemSelectedListener(this);
+
+            ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, R.layout.spinner_item, nodeNameList);
+            adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner_toNode.setAdapter(adapter2);
+            spinner_toNode.setOnItemSelectedListener(this);
+
+            textView_Name.setVisibility(View.INVISIBLE);
+            textView_Shape.setVisibility(View.INVISIBLE);
+            textView_fromNode.setVisibility(View.VISIBLE);
+            textView_toNode.setVisibility(View.VISIBLE);
+
+            spinner1.setVisibility(View.INVISIBLE);
+            spinner_fromNode.setVisibility(View.VISIBLE);
+            spinner_toNode.setVisibility(View.VISIBLE);
+
+            editText_nodeCount.setVisibility(View.INVISIBLE);
+            editText_myDot.setVisibility(View.VISIBLE);
+            editText_nodeName.setVisibility(View.INVISIBLE);
+
+            button_submitCount.setVisibility(View.INVISIBLE);
+            button_addNode.setVisibility(View.INVISIBLE);
+            button_addEdge.setVisibility(View.VISIBLE);
         }
     }
 
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+        nodeShape = parent.getItemAtPosition(position).toString();
     }
 
     @Override
